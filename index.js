48d3178401a477aee16ef100552014e025988d7e
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-05-29 01:40:46 -0500

    added find endpoint

--- index.js
+++ index.js
@@ -29,9 +29,37 @@ function findById(id){
   return db('zoos').where({ id })
 }
 
-// endpoints here
+// test
+async function execute(){
+  const zoo = await findZoos()
+  console.log(zoo)
+}
 
+// endpoints here
+server.get('/api', (req, res) => {
+  res.send(`
+  <h2>
+      WEBDB2 
+  </h2>`)
+})
+
+server.get('/api/zoos', async (req, res) => {
+  try{
+      const zooList = await db.findZoos();
+      res.status(200).json(zooList)
+  } catch (error) {
+      res.status(500).json({
+          message: 'The account list could not be retrieved.'
+      })
+  }
+})
+
+
+
+//server
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+execute()
